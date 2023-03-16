921f78d4d15b818202e8d3f58db3f059db38856f
DimitarZlatev10 <aguerofc@abv.bg>
2023-03-16 22:34:34 +0200

    added product details , addComment and deleteComment

--- controllers/product.js
+++ controllers/product.js
@@ -1,5 +1,79 @@
+const {
+  getProductById,
+  addComment,
+  deleteComment,
+} = require("../services/product");
+const { mapErrors } = require("../utils/errorDisplayer");
+const {
+  Types: { ObjectId },
+} = require("mongoose");
+const { isUser } = require("../middlewares/guards");
+
 const router = require("express").Router();
 
+router.get("/details/:id", async (req, res) => {
+  const product = await getProductById(req.params.id);
 
+  if (product.comments.length > 0) {
+    if (req.session.user) {
+      product.comments.forEach((c) => {
+        if (c.owner == req.session.user._id) {
+          c.isOwner = true;
+        }
+      });
+    }
+
+    const productId = product._id;
+    let totalRating = 0;
+    let totalReviews = product.comments.length;
+    product.comments.forEach((r) => {
+      totalRating += Number(r.rating);
+    });
+    let finalRating = (totalRating / totalReviews).toFixed(1);
+    res.render("details", {
+      title: `${product.title}`,
+      product,
+      finalRating,
+      totalReviews,
+      productId,
+    });
+  } else {
+    res.render("details", { title: `${product.title}`, product });
+  }
+});
+
+router.post("/addComment/:id", isUser(), async (req, res) => {
+  const commentInfo = {
+    _id: new ObjectId(),
+    username: `${req.session.user.firstName} ${req.session.user.lastName}`,
+    rating: req.body.rating,
+    comment: req.body.comment,
+    owner: req.session.user._id,
+    productId: req.params.id,
+    userImage: req.session.user.image,
+  };
+
+  try {
+    await addComment(req.params.id, commentInfo);
+    res.redirect("/details/" + req.params.id);
+  } catch (err) {
+    console.error(err);
+    const errors = mapErrors(err);
+    res.render("details", { title: "Details", errors });
+  }
+});
+
+router.get(
+  "/deleteComment/:productId/:commentId",
+  isUser(),
+  async (req, res) => {
+    try {
+      await deleteComment(req.params.productId, req.params.commentId);
+      res.redirect("/details/" + req.params.productId);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+);
 
 module.exports = router;
--- services/product.js
+++ services/product.js
@@ -4,6 +4,32 @@ async function getAllPosts() {
   return Product.find({}).lean();
 }
 
+async function getProductById(productId) {
+  return Product.findById(productId).lean();
+}
+
+async function addComment(productId, comment) {
+  const existingProduct = await Product.findById(productId);
+
+  existingProduct.comments.push(comment);
+  await existingProduct.save();
+}
+
+async function deleteComment(productId, commentId) {
+  let product = await Product.findById(productId);
+
+  product.comments.forEach((p, i) => {
+    if (p._id == commentId) {
+      product.comments.splice(i, 1);
+    }
+  });
+
+  await product.save();
+}
+
 module.exports = {
   getAllPosts,
+  getProductById,
+  addComment,
+  deleteComment,
 };
